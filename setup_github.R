9d57384efd16d4d27cb16017984000484c8885a6
ms86fyko <ms86fyko@studserv.uni-leipzig.de>
2024-11-08 09:48:09 +0100

    commit random change

--- setup_github.R
+++ setup_github.R
@@ -10,6 +10,7 @@ git_vaccinate()
 usethis::use_git()
 use_github()
 git_vaccinate()
-git_default_branch_rename()
-git_default_branch_configure(name="main")
+git_default_branch_rename() # rename the main branch
+git_default_branch_configure(name="main") # set new default
 
+#change in the file
